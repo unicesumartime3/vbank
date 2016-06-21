dc0924f78fcbb45c11d7caf3463afefa052bf6a6
Flávia Ferreira <flaviahferreirah@gmail.com>
2016-06-20 23:48:41 -0300

    Criado o teste do service da classe MotivoRejeicao, criado os metodos save, findById e remove da classe MotivoRejeicaoService e alterado o nome dos testes da classe MotivoRejeicaoRepositoryTest.

--- jee-blank/src/main/java/br/com/rp/services/MotivoRejeicaoService.java
+++ jee-blank/src/main/java/br/com/rp/services/MotivoRejeicaoService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import br.com.rp.domain.Conta;
 import br.com.rp.domain.MotivoRejeicao;
 import br.com.rp.repository.MotivoRejeicaoRepository;
 
@@ -32,5 +33,17 @@ public class MotivoRejeicaoService {
 	
 	public List<MotivoRejeicao> getAll() {
 		return motivoRejeicaoRepository.getAll();
+	}
+	
+	public MotivoRejeicao save(MotivoRejeicao motivo){
+		return motivoRejeicaoRepository.save(motivo);
+	}
+	
+	public MotivoRejeicao findById(Long id){
+		return motivoRejeicaoRepository.findById(id);
+	}
+	
+	public void remove(Long id){
+		motivoRejeicaoRepository.remove(id);
 	}
 }
--- jee-blank/src/test/java/br/com/rp/repository/service/MotivoRejeicaoServiceTest.java
+++ jee-blank/src/test/java/br/com/rp/repository/service/MotivoRejeicaoServiceTest.java
@@ -1,4 +1,4 @@
-package br.com.rp.repository;
+package br.com.rp.repository.service;
 
 import javax.ejb.EJB;
 
@@ -8,46 +8,47 @@ import org.junit.Test;
 
 import br.com.rp.AbstractTest;
 import br.com.rp.domain.MotivoRejeicao;
+import br.com.rp.services.MotivoRejeicaoService;
 
-public class MotivoRejeicaoRepositoryTest extends AbstractTest {
+public class MotivoRejeicaoServiceTest extends AbstractTest {
 
 	@EJB
-	private MotivoRejeicaoRepository motivoRejeicaoRepository;
+	private MotivoRejeicaoService motivoRejeicaoservice;
 
 	@Test
 	public void deveInserirMotivoComSucesso() {
 		MotivoRejeicao motivo = new MotivoRejeicao();
 		motivo.setDsMotivo("Renda insuficiente");
-		motivoRejeicaoRepository.save(motivo);
+		motivoRejeicaoservice.save(motivo);
 		Assert.assertNotNull(motivo.getId());
 	}
 
 	@Test
 	@UsingDataSet("db/motivo_rejeicao.xml")
 	public void deveCompararDescricaoMotivoRejeicao() {
-		Assert.assertEquals("Renda insuficiente", motivoRejeicaoRepository.findById(100001L).getDsMotivo());
+		Assert.assertEquals("Renda insuficiente", motivoRejeicaoservice.findById(100001L).getDsMotivo());
 	}
 
 	@Test
 	@UsingDataSet("db/motivo_rejeicao.xml")
 	public void deveRetornarDoisRegistros() {
-		Assert.assertEquals(2, motivoRejeicaoRepository.getAll().size());
+		Assert.assertEquals(2, motivoRejeicaoservice.getAll().size());
 	}
 
 	@Test
 	@UsingDataSet("db/motivo_rejeicao.xml")
 	public void deveRemoverMotivoComSucesso() {
-		motivoRejeicaoRepository.remove(100001L);
-		Assert.assertEquals(1, motivoRejeicaoRepository.getAll().size());
+		motivoRejeicaoservice.remove(100001L);
+		Assert.assertEquals(1, motivoRejeicaoservice.getAll().size());
 	}
 
 	@Test
 	@UsingDataSet("db/motivo_rejeicao.xml")
 	public void deveAtualizarMotivoComSucesso() {
-		MotivoRejeicao motivo = motivoRejeicaoRepository.findById(100001L);
+		MotivoRejeicao motivo = motivoRejeicaoservice.findById(100001L);
 		motivo.setDsMotivo("Horário inválido para operação");
-		motivoRejeicaoRepository.save(motivo);
-		Assert.assertEquals("Horário inválido para operação", motivoRejeicaoRepository.findById(100001L).getDsMotivo());
+		motivoRejeicaoservice.save(motivo);
+		Assert.assertEquals("Horário inválido para operação", motivoRejeicaoservice.findById(100001L).getDsMotivo());
 	}
 
 }
