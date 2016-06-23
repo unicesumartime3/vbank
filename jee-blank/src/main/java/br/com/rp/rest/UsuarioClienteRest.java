28c47c7dfc15fe337bcde934012541cf858e1e45
Flávia Ferreira <flaviahferreirah@gmail.com>
2016-06-23 17:42:25 -0300

    Criado o REST do UsuarioCliente e os testes da classe UsuarioClienteRestTest.

--- jee-blank/src/main/java/br/com/rp/rest/UsuarioClienteRest.java
+++ jee-blank/src/main/java/br/com/rp/rest/UsuarioClienteRest.java
@@ -3,10 +3,18 @@ package br.com.rp.rest;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import br.com.rp.domain.UsuarioCliente;
+import br.com.rp.domain.UsuarioFuncionario;
 import br.com.rp.services.UsuarioClienteService;
 
 /**
@@ -25,13 +33,46 @@ import br.com.rp.services.UsuarioClienteService;
  *
  */
 
-@Path(value = "/usuariocliente")
+@Path("/usuario/cliente")
+@Produces("application/json")
 public class UsuarioClienteRest {
 	
 	@EJB
 	private UsuarioClienteService usuarioClienteService;
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/save")
+	public UsuarioCliente save(UsuarioCliente usuarioCliente) {
+			return usuarioClienteService.save(usuarioCliente);
+	}
+	
+	@PUT
+	@Path("/update/{id}")
+	public UsuarioCliente update(@PathParam("id") Long id, UsuarioCliente usuarioCliente) {
+		UsuarioCliente usuarioClienteResult = usuarioClienteService.findById(id);
+		usuarioClienteResult.setNome(usuarioCliente.getNome());
+		usuarioClienteResult.setSenha(usuarioCliente.getSenha());
+		usuarioClienteResult.setCliente(usuarioCliente.getCliente());
+		
+		return usuarioClienteService.save(usuarioClienteResult);
+	}
+	
+	@DELETE
+	@Path("/remove/{id}")
+	public void remove(@PathParam("id") Long id) {
+		usuarioClienteService.remove(id);
+	}
 	
 	@GET
+	@Path("/findById/{id}")
+	public UsuarioCliente findById(@PathParam("id") Long id) {
+		return usuarioClienteService.findById(id);
+	}
+	
+	@GET
+	@Path("/getAll")
 	public List<UsuarioCliente> getAll() {
 		return usuarioClienteService.getAll();
 	}
---
+++ jee-blank/src/test/java/br/com/rp/rest/UsuarioClienteRestTest.java
@@ -0,0 +1,120 @@
+package br.com.rp.rest;
+
+import java.util.List;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.arquillian.persistence.Cleanup;
+import org.jboss.arquillian.persistence.CleanupStrategy;
+import org.jboss.arquillian.persistence.UsingDataSet;
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.rp.AbstractTest;
+import br.com.rp.domain.Cliente;
+import br.com.rp.domain.UsuarioCliente;
+
+@Cleanup(strategy = CleanupStrategy.USED_TABLES_ONLY)
+public class UsuarioClienteRestTest extends AbstractTest {
+
+	private static final String URL_BASE = "http://localhost:8080/vbank/api";
+
+	@Test
+	@UsingDataSet("db/usuario_cliente.xml")
+	public void deveSalvarUsuarioClienteComSucesso() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(URL_BASE + "/cliente/findById/100001");
+		Response response = target.request().get();
+		Assert.assertEquals(Integer.valueOf(200), Integer.valueOf(response.getStatus()));
+		Cliente cliente = response.readEntity(Cliente.class);
+		Assert.assertNotNull(cliente);
+
+		target = client.target(URL_BASE + "/usuario/cliente/save");
+		response = target.request().accept(MediaType.APPLICATION_JSON)
+				.post(Entity.entity(new UsuarioCliente("Flavia Ferreira", "123456", cliente),
+						MediaType.APPLICATION_JSON));
+		Assert.assertEquals(Integer.valueOf(200), Integer.valueOf(response.getStatus()));
+		UsuarioCliente usuarioCliente = response.readEntity(UsuarioCliente.class);
+		Assert.assertNotNull(usuarioCliente);
+		Assert.assertEquals("Flavia Ferreira", usuarioCliente.getNome());
+	}
+
+	@Test
+	@UsingDataSet("db/usuario_cliente.xml")
+	public void deveAlterarUsuarioClienteComSucesso() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(URL_BASE + "/usuario/cliente/findById/100001");
+		Response response = target.request().get();
+		Assert.assertEquals(Integer.valueOf(200), Integer.valueOf(response.getStatus()));
+		UsuarioCliente usuarioCliente = response.readEntity(UsuarioCliente.class);
+		Assert.assertEquals("Christian Marchiori", usuarioCliente.getNome());
+
+		target = client.target(URL_BASE + "/cliente/findById/100001");
+		response = target.request().get();
+		Assert.assertEquals(Integer.valueOf(200), Integer.valueOf(response.getStatus()));
+		Cliente cliente = response.readEntity(Cliente.class);
+		Assert.assertNotNull(cliente);
+
+		target = client.target(URL_BASE + "/usuario/cliente/update/100001");
+		response = target.request()
+				.put(Entity.entity(new UsuarioCliente("Flavia Ferreira","123456", cliente),
+						MediaType.APPLICATION_JSON));
+		Assert.assertEquals(Integer.valueOf(200), Integer.valueOf(response.getStatus()));
+		UsuarioCliente usuarioClienteResult = response.readEntity(UsuarioCliente.class);
+		Assert.assertNotNull(usuarioClienteResult);
+		Assert.assertEquals("Flavia Ferreira", usuarioClienteResult.getNome());
+	}
+
+	@Test
+	@UsingDataSet("db/usuario_cliente.xml")
+	public void deveRemoverUsuarioClienteComSucesso() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(URL_BASE + "/usuario/cliente/remove/100001");
+		Response response = target.request().delete();
+		Assert.assertEquals(Integer.valueOf(204), Integer.valueOf(response.getStatus()));
+
+		target = client.target(URL_BASE + "/usuario/cliente/findById/100001");
+		response = target.request().get();
+		Assert.assertEquals(Integer.valueOf(204), Integer.valueOf(response.getStatus()));
+		UsuarioCliente usuarioCliente = response.readEntity(UsuarioCliente.class);
+		Assert.assertNull(usuarioCliente);
+	}
+
+	@Test
+	@UsingDataSet("db/usuario_cliente.xml")
+	public void deveRetornarUsuarioCliente() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(URL_BASE + "/usuario/cliente/findById/100001");
+		Response response = target.request().get();
+		Assert.assertEquals(Integer.valueOf(200), Integer.valueOf(response.getStatus()));
+		UsuarioCliente usuarioCliente = response.readEntity(UsuarioCliente.class);
+		Assert.assertNotNull(usuarioCliente);
+	}
+
+	@Test
+	@UsingDataSet("db/usuario_cliente.xml")
+	public void deveCompararUsuarioClienteNome() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(URL_BASE + "/usuario/cliente/findById/100001");
+		Response response = target.request().get();
+		Assert.assertEquals(Integer.valueOf(200), Integer.valueOf(response.getStatus()));
+		UsuarioCliente usuarioCliente = response.readEntity(UsuarioCliente.class);
+		Assert.assertEquals("Christian Marchiori", usuarioCliente.getNome());
+	}
+
+	@Test
+	@UsingDataSet("db/usuario_cliente.xml")
+	public void deveRetornaDoisUsuariosClientes() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target(URL_BASE + "/usuario/cliente/getAll");
+		Response response = target.request().get();
+		Assert.assertEquals(Integer.valueOf(200), Integer.valueOf(response.getStatus()));
+		List<UsuarioCliente> usuariosCliente = (List<UsuarioCliente>) response.readEntity(List.class);
+		Assert.assertEquals(2, usuariosCliente.size());
+	}
+}
